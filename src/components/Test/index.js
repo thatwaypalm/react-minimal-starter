987c7abc0f791a58c9d6c520dbb7ed008f0f5a06
thatway <thatwaypalm@gmail.com>
2018-05-05 17:09:08 +0300

    minimal react boilerplate

---
+++ src/components/Test/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "./Test.css"
+
+const Test = () => {
+  return (
+    <div className={style.testClass}>
+      Hello Test Component :)
+    </div>
+  )
+}
+
+export default Test;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Test from './components/Test';
+
+const Index = () => {
+  return <Test />;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
